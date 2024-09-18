b4cc690a85e6bda21ce57c74744199c9fdb27e0c
Shuvo <115610014+commoner02@users.noreply.github.com>
2024-09-19 04:04:38 +0600

    Add files via upload
    Find frequency of a number in array

---
+++ lab02_frequency_count.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class lab02_frequency_count{
+   public static  void main(String[] args) {
+    System.out.println("Enter Array Size:");
+    Scanner var=new Scanner(System.in);
+    int x=var.nextInt();
+    int[] arr=new int[x];
+    System.out.println("Enter Array Elements:");
+    for(int i=0;i<x;i++){
+        int v=var.nextInt();
+        arr[i]=v;
+    }
+    System.out.println("Search Element :");
+    int s=var.nextInt();
+    int cnt=0;
+    for(int i=0;i<x;i++){
+        if(s==arr[i]){
+          cnt++;
+        }
+    }
+    System.out.println("Count of "+s+ " is "+cnt);
+   }
+   
+}
